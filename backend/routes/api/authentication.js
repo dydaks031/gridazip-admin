af4349dd314af2c1d07b5eec3851b5b0cf2156a2
ohw <ohw@gridazip.com>
2018-04-26 17:44:58 +0900

    admin api seperate work

--- backend/routes/api/authentication.js
+++ backend/routes/api/authentication.js
@@ -1,13 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const paginationService = require('../../services/pagination/main');
-const filterService = require('../../services/filter/main');
 const knexBuilder = require('../../services/connection/knex');
-const cryptoHelper = require('../../services/crypto/helper');
 const resHelper = require('../../services/response/helper');
-const resModel = require('../../services/response/model');
-const randomHelper = require('../../services/random/helper');
-const s3Helper = require('../../services/s3/helper');
 const jwtHelper = require('../../services/jwt/helper')
 
 router.post('/login', (req, res) => {
