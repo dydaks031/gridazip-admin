7636cbd62e4d9aeb7df3d956ed91ba6dce2dcd34
hyungwon.kang <hyungwon.kang@gridazip.com>
2018-06-27 16:59:13 +0900

    update: added pm2 ecosystem.json, fixed typo

--- frontend/client/views/estimate/estimateSheet.vue
+++ frontend/client/views/estimate/estimateSheet.vue
@@ -89,8 +89,8 @@
               <tr v-for="labor in viewerData.labor" v-show="labor.labor_costs !== 0">
                 <td>{{labor.ct_name}}</td>
                 <td>{{labor.cp_name}}</td>
-                <td>{{labor.cpd_name}}</td>
                 <td>{{labor.rt_name}}</td>
+                <td>{{labor.cpd_name}}</td>
                 <td>{{addCommas(labor.labor_costs)}}</td>
               </tr>
               </tbody>
@@ -273,7 +273,6 @@
           }
         }
 
-        console.log(mergeCount)
         const firstMeetPk = {
           place: {
 
