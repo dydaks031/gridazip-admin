bca54062dc2a76d9df620c3284ff3eadd541a3c4
hyungwon.kang <hyungwon.kang@gridazip.co.kr>
2018-09-04 10:31:30 +0900

    webhook Test

--- backend/routes/api/webhook.js
+++ backend/routes/api/webhook.js
@@ -7,6 +7,31 @@ const resHelper = require('../../services/response/helper');
 
 router.post('/listener', (req, res) => {
   console.log(req.body)
+  const userInfo = req.body.refers.veil;
+  const mobileNumber = userInfo.mobileNumber;
+
+  if (!mobileNumber) {
+    res.json(resHelper.getJson({
+      msg: 'NOT INSERTED'
+    }));
+    return
+  }
+
+  const insertData = {
+    ch_id: userInfo.id,
+    ch_name: userInfo.name,
+    ch_phone: userInfo.mobileNumber,
+    ch_latitude: userInfo.latitude,
+    ch_longitude: userInfo.longitude,
+    ch_segment: userInfo.segment,
+    ch_locale: userInfo.locale,
+    ch_country: userInfo.country
+  }
+  console.log(JSON.stringify(insertData))
+
+  res.json(resHelper.getJson({
+    msg: 'OK'
+  }));
 });
 
 module.exports = router;
