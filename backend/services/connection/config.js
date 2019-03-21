e537741d9d1d42256519be3745c0cac4cacfa219
ohw <ohw@gridazip.com>
2019-03-21 14:18:26 +0900

    build and deploy.

--- backend/services/connection/config.js
+++ backend/services/connection/config.js
@@ -8,6 +8,15 @@ const Config = {
     secure: true
   },
 
+  production: {
+    host: 'gridazip-site.cl6yvvzfiwjs.ap-northeast-2.rds.amazonaws.com',
+    port: 3306,
+    user: 'root',
+    password: '6d40ceaefe41c92c503691da72ecfc9e',
+    database: 'gridazip',
+    secure: true
+  },
+
   development: {
     host: 'gridaziptest.cl6yvvzfiwjs.ap-northeast-2.rds.amazonaws.com',
     port: 3306,
