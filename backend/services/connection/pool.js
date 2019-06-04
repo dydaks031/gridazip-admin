479b01145a7fee27dde23aa7c8045c703906080c
ohw <ohw@gridazip.com>
2019-06-04 15:26:02 +0900

    local db pool added.

--- backend/services/connection/pool.js
+++ backend/services/connection/pool.js
@@ -3,9 +3,13 @@ const helper = require('./helper');
 
 const defaultConnectionInfo = helper.getInfo('default');
 const developmentConnectionInfo = helper.getInfo('development');
+const localConnectionInfo = helper.getInfo('local');
 let innerPool = null;
 if (process.env.NODE_ENV === 'development') {
   innerPool = mysql.createPool(developmentConnectionInfo);
+}
+else if (process.env.NODE_ENV === 'local') {
+  innerPool = mysql.createPool(localConnectionInfo);
 }
 else {
   innerPool = mysql.createPool(defaultConnectionInfo);
